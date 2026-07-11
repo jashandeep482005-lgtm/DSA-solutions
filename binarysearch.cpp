7eb5db8c2749144679032dc17615a54bd455858f
jashandeep <jashandeep482005@gmail.com>
2026-07-11 12:03:18 +0530

    Add Search Insert Position solution

---
+++ binarysearch.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int binarySearch(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return low;
+    }
+
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return binarySearch(nums, target);
+    }
+};
